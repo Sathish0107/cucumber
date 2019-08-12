5cb31539148022708b6fc8e77caa1015fc41cf73
Sathish0107 <sathishbaskaranr@gmail.com>
2019-08-12 20:09:02 +0530

    scenario outline

--- features/addcustomer.feature
+++ features/addcustomer.feature
@@ -2,15 +2,27 @@
 Feature: Title of your feature
   I want to use this template for my feature file
 
-  @tag1
-  Scenario: Title of your scenario
-    Given launch the browser
-    When enter the customer details
-  |fname|sathish|
-  |lname|baskaran|
-  |email|sathishbaskaranr@gmail.com|
-  |address|chennai|
-  |phone|9840076968|
-    Then I validate the outcomes
+#@tag1
+ #Scenario: Title of your scenario
+   # Given launch the browser
+    
+    
+ #   When enter the customer details
+  #|fname|sathish|
+  #|lname|baskaran|
+  #|email|sathishbaskaranr@gmail.com|
+  #|address|chennai|
+  #|phone|9840076968|
+  #  Then I validate the outcomes
  
+@tag2
+Scenario Outline: Rutime
+Given launch the brwoser
+When enter the customer details "<fname>", "<lname>","<email>","<address>","<phone>"
+Then Validate outcome
 
+Examples:
+|fname|lname|email|address|phone|
+|sathish|baskaran|abc@gmail.com|chennai|9840076968|
+|divya|baskaran|bca@gmail.com|chennai|787455575|
+|siva|thimu|xyz@gmail.com|chennai|885247963|
--- src/test/java/hex/ama/cucumber/Addcustomer.java
+++ src/test/java/hex/ama/cucumber/Addcustomer.java
@@ -18,40 +18,73 @@ public class Addcustomer {
 
 	WebDriver driver;
 
-	@Given("launch the browser")
-	public void launch_the_browser() {
+	/*
+	 * @Given("launch the browser") public void launch_the_browser() {
+	 * 
+	 * System.setProperty("webdriver.chrome.driver",
+	 * "C:\\Users\\Asus\\eclipse-workspace\\cucumber\\driver\\chromedriver.exe");
+	 * 
+	 * driver = new ChromeDriver();
+	 * 
+	 * driver.get("http://demo.guru99.com/telecom/");
+	 * driver.findElement(By.xpath("//*[text()='Add Customer']")).click();
+	 * driver.findElement(By.xpath("//*[text()='Done']")).click(); }
+	 * 
+	 * 
+	 * @When("enter the customer details") public void
+	 * enter_the_customer_details(io.cucumber.datatable.DataTable dataTable) {
+	 * 
+	 * Map<String, String> datas= dataTable.asMap(String.class, String.class);
+	 * 
+	 * driver.findElement(By.id("fname")).sendKeys(datas.get("fname"));
+	 * driver.findElement(By.id("lname")).sendKeys(datas.get("lname"));
+	 * driver.findElement(By.id("email")).sendKeys(datas.get("email"));
+	 * driver.findElement(By.xpath("(//*[@id='message'])[2]")).sendKeys(datas.get(
+	 * "address"));
+	 * driver.findElement(By.id("telephoneno")).sendKeys(datas.get("phone"));
+	 * driver.findElement(By.xpath("//*[@type='submit']")).click(); throw new
+	 * cucumber.api.PendingException();
+	 * 
+	 * }
+	 * 
+	 * 
+	 * @Then("I validate the outcomes") public void i_validate_the_outcomes() {
+	 * Assert.assertTrue((driver.findElement(By.xpath("//*[text()='Customer ID']")).
+	 * isDisplayed()));
+	 * 
+	 * }
+	 */
 
-		System.setProperty("webdriver.chrome.driver",
-				"C:\\Users\\Asus\\eclipse-workspace\\cucumber\\driver\\chromedriver.exe");
-
-		driver = new ChromeDriver();
-
-		driver.get("http://demo.guru99.com/telecom/");
-		driver.findElement(By.xpath("//*[text()='Add Customer']")).click();
-		 driver.findElement(By.xpath("//*[text()='Done']")).click();
-	}
-	
-	@When("enter the customer details")
-	public void enter_the_customer_details(io.cucumber.datatable.DataTable dataTable) {
+	@Given("launch the brwoser")
+	public void launch_the_brwoser() {
+	   
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Asus\\eclipse-workspace\\cucumber\\driver\\chromedriver.exe");
+				  driver = new ChromeDriver();
+				  driver.get("http://demo.guru99.com/telecom/");
+				driver.findElement(By.xpath("//*[text()='Add Customer']")).click();
+				 driver.findElement(By.xpath("//*[text()='Done']")).click(); 
 		
-		Map<String, String> datas=	dataTable.asMap(String.class, String.class);
+	}
+
+	@When("enter the customer details {string}, {string},{string},{string},{string}")
+	public void enter_the_customer_details(String A, String B, String C, String D, String E) {
+	  
+		driver.findElement(By.id("fname")).sendKeys(A);
+		 driver.findElement(By.id("lname")).sendKeys(B);
+		 driver.findElement(By.id("email")).sendKeys(C);
+		 driver.findElement(By.xpath("(//*[@id='message'])[2]")).sendKeys(D);
+		 driver.findElement(By.id("telephoneno")).sendKeys(E);
+		  driver.findElement(By.xpath("//*[@type='submit']")).click();
 		
-	     driver.findElement(By.id("fname")).sendKeys(datas.get("fname"));
-		 driver.findElement(By.id("lname")).sendKeys(datas.get("lname"));
-		 driver.findElement(By.id("email")).sendKeys(datas.get("email"));
-		 driver.findElement(By.xpath("(//*[@id='message'])[2]")).sendKeys(datas.get("address"));
-		 driver.findElement(By.id("telephoneno")).sendKeys(datas.get("phone"));
-		 driver.findElement(By.xpath("//*[@type='submit']")).click();
-	    throw new cucumber.api.PendingException();
 	}
 
-	@Then("I validate the outcomes")
-	public void i_validate_the_outcomes() {
-		 Assert.assertTrue((driver.findElement(By.xpath("//*[text()='Customer ID']")).
-				 isDisplayed()));
-				
-
+	@Then("Validate outcome")
+	public void validate_outcome() {
+		
+		Assert.assertTrue((driver.findElement(By.xpath("//*[text()='Customer ID']")).isDisplayed()));
+	   
 	}
+
 	
 	
 }
